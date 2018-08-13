b76eba85228b2326dc609baf6034ce55782453fc
Mendy <mendybeltcohen@gmail.com>
2018-08-13 20:10:38 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,9 @@ def reverse_each_word(rev)
   # rev.split.collect do |x|
   #   x.reverse
   # end
-  # arr = []
-   rev.split.collect do |x|
-    x.split.join.flatten
-   end
+  arr = []
+  rev.split.collect do |x|
+   arr << x.reverse
+ end
+ arr
 end
